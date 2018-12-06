c5b3740c7e38c3a2b3a32e906261142e0bbd2875
FurFurJohn <44696601+FurFurJohn@users.noreply.github.com>
2018-12-06 14:40:15 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -25,7 +25,7 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   inv <- x$getinverse()
-  if(!is.null(m)) {
+  if(!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
